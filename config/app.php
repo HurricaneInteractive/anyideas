c328c397b110ca6e4424c3a06882994984147a21
Luke Secomb <luke.secomb@live.com>
2018-08-18 13:12:39 +1000

    remove .env variables

--- config/app.php
+++ config/app.php
@@ -104,7 +104,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'inesindinemwanawabambuyabakoiwe'),
+    'key' => env('APP_KEY'),
 
     'cipher' => 'AES-256-CBC',
 
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -39,7 +39,7 @@
                             </div>
                         </div>
 
-                        <div class="form-group row">
+                        <!-- <div class="form-group row">
                             <label for="car" class="col-md-4 col-form-label text-md-right">{{ __('Personal Car Type') }}</label>
 
                             <div class="col-md-6">
@@ -51,7 +51,7 @@
                                     </span>
                                 @endif
                             </div>
-                        </div>
+                        </div> -->
 
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
