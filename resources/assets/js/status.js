b21959b1aca91d58fa661e439730fc8c3b313e18
Luke Secomb <luke.secomb@live.com>
2018-08-27 14:50:58 +1000

    added status options

---
+++ resources/assets/js/status.js
@@ -0,0 +1,10 @@
+satus = {
+  "Ideation": "#222222",
+  "Planning": "#222222",
+  "Creating": "#222222",
+  "Evaluating": "#222222",
+  "Refining": "#222222",
+  "Completed": "#222222",
+  "On Hold": "#222222",
+  "Abandoned": "#ffffff"
+}
