4d4d99eb345c6b67c183db1b12e098c30a581acc
Luke Secomb <luke.secomb@live.com>
2018-09-04 12:16:34 +1000

    renaming / editing migration tables

--- database/migrations/2018_08_30_193638_create_updates_posts_table.php
+++ database/migrations/2018_08_30_193638_create_updates_posts_table.php
@@ -21,7 +21,6 @@ class CreateUpdatesPostsTable extends Migration
             $table->foreign('idea_id')->references('id')->on('ideas');
             
             $table->increments('id');
-            $table->string('title');
             $table->mediumText('message');
             $table->mediumInteger('darts')->default('0');
             $table->timestamps();
