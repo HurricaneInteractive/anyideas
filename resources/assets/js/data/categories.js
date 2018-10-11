5d68beffe38d8f310de29e31083ef6d3f22db77a
Luke Secomb <luke.secomb@live.com>
2018-10-11 16:42:44 +1100

    moved and properly formatted js variables

---
+++ resources/assets/js/data/categories.js
@@ -0,0 +1,13 @@
+const categories = [
+    "Arts",
+    "Entertainment",
+    "Culture",
+    "Life",
+    "Well-being",
+    "Industry",
+    "Business",
+    "Innovation",
+    "Technology",
+    "Science"
+]
+export default categories
---
+++ resources/assets/js/data/icons.js
@@ -0,0 +1,74 @@
+const icons = {
+  "logo_small": `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 27 19.722">
+      <g id="anyideas-icon" transform="translate(0.102 -0.088)">
+        <path id="Path_58" data-name="Path 58" d="M576.462,365.21l-1.2.262a1.145,1.145,0,1,0,.475,2.231l3.443-.737Z" transform="translate(-569.423 -355.517)"/>
+        <path id="Path_59" data-name="Path 59" d="M571.372,361.91l-1.2.262a1.145,1.145,0,1,0,.475,2.231l3.443-.737Z" transform="translate(-569.27 -355.418)"/>
+        <path id="Path_60" data-name="Path 60" d="M3.88,6.33h0v0Z" transform="translate(-0.12 -0.187)"/>
+        <path id="Path_61" data-name="Path 61" d="M596.334,374.2v-.068a.878.878,0,0,0,0-.155.248.248,0,0,0,0-.068.754.754,0,0,0,0-.136.248.248,0,0,0,0-.068h0l-3.88-6.528h0v-.048h-.96l-.058.058h0l-.049.078h0l-1.212,1.882-7.032-4.559-1.086-5.083a1.146,1.146,0,1,0-2.241.485l.611,2.842-2.221-1.436-1.1-5.083a1.145,1.145,0,1,0-2.231.475l.97,4.423h0l.339,1.581,12.765,8.264-.65.97h0l-.582.9h0a.7.7,0,0,0-.087.194h0a.306.306,0,0,0,0,.078h0v.223a.126.126,0,0,0,0,.058s0,0,0,.048v.223a.134.134,0,0,0,0,.058h0v.068h0v.078h0l.048.068h0l.058.058h0l.048.058h.417l7.556.892h-.184l.136-.068h0l.116-.078.068-.068.078-.087.068-.087.049-.1a.519.519,0,0,0,0-.107.406.406,0,0,0,0-.1V374.2h0A.417.417,0,0,0,596.334,374.2Z" transform="translate(-569.44 -355.22)"/>
+      </g>
+    </svg>`
+  ,
+  "search": `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 15">
+      <defs>
+        <style>
+          .cls-1 {
+            fill: none;
+            stroke: #000;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+          }
+        </style>
+      </defs>
+      <g id="search" transform="translate(-242.5 -35.5)">
+        <path id="Union_1" data-name="Union 1" class="cls-1" d="M-438.378,10.622a6.2,6.2,0,0,1-4.4,1.823A6.222,6.222,0,0,1-449,6.222,6.222,6.222,0,0,1-442.778,0a6.223,6.223,0,0,1,6.222,6.222,6.2,6.2,0,0,1-1.823,4.4L-435,14Z" transform="translate(692 36)"/>
+      </g>
+    </svg>
+  `,
+  "user": `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 15 16.167">
+      <defs>
+        <style>
+          .cls-1 {
+            fill: none;
+            stroke: #000;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+          }
+        </style>
+      </defs>
+      <g id="user" transform="translate(-330.5 -36.5)">
+        <g id="Group_2" data-name="Group 2">
+          <path id="Path_1" data-name="Path 1" class="cls-1" d="M18,20.056V18.37A3.437,3.437,0,0,0,14.5,15h-7A3.437,3.437,0,0,0,4,18.37v1.685" transform="translate(327 32.111)"/>
+          <ellipse id="Ellipse_1" data-name="Ellipse 1" class="cls-1" cx="3.5" cy="3.37" rx="3.5" ry="3.37" transform="translate(334.5 37)"/>
+        </g>
+      </g>
+    </svg>
+  `,
+  "plus": `
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 11 11">
+      <defs>
+        <style>
+          .cls-1 {
+            fill: none;
+            stroke: #000;
+            stroke-linecap: round;
+            stroke-linejoin: round;
+          }
+        </style>
+      </defs>
+      <g id="plus" transform="translate(-380.5 -123.5)">
+        <path id="Union_2" data-name="Union 2" class="cls-1" d="M-424,88.687h0v0h0v0Z" transform="translate(810 40.312)"/>
+      </g>
+    </svg>
+  `,
+  "exit": "#222222",
+  "refresh": "#222222",
+  "social": {
+    "facebook": "value",
+    "instagram": "value",
+    "twitter": "value",
+  }
+}
+export default icons
