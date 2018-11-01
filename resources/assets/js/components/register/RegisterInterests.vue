fbc219a2599c15d6dea7ea319c92171282ac77b0
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-02 07:57:58 +1100

    Email, Password & initial interest view

---
+++ resources/assets/js/components/register/RegisterInterests.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="step step-1">
+        <blockquote>
+            <p>“If you always do what interests you, at least one person is pleased”</p>
+            <cite>KATHARINE HEPBURN</cite>
+        </blockquote>
+        <div class="interest-selector">
+            
+        </div>
+        <div class="form-block">
+            <div class="input-field">
+                <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    v-on:input="onChange"
+                    :class="emailHasContent"
+                    autocomplete="off"
+                    autofocus
+                />
+                <label for="email">Email</label>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'RegisterEmail',
+        props: ['email'],
+        methods: {
+            onChange(e) {
+                this.$emit('onchange', e)
+            }
+        },
+        computed: {
+            emailHasContent: function() {
+                return {
+                    'has-content': this.email
+                }
+            }
+        }
+    }
+</script>
