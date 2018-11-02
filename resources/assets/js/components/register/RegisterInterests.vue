75df63ea0d0f6f4d2c9e90ef176ef90850648b01
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-02 22:55:27 +1100

    Added RegisterInterests component with styling and change event

--- resources/assets/js/components/register/RegisterInterests.vue
+++ resources/assets/js/components/register/RegisterInterests.vue
@@ -5,39 +5,68 @@
             <cite>KATHARINE HEPBURN</cite>
         </blockquote>
         <div class="interest-selector">
-            
-        </div>
-        <div class="form-block">
-            <div class="input-field">
-                <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    v-on:input="onChange"
-                    :class="emailHasContent"
-                    autocomplete="off"
-                    autofocus
-                />
-                <label for="email">Email</label>
-            </div>
+            <p><strong>Select Your Interests</strong> (max. of 3)</p>
+            <ul class="categories">
+                <li
+                    v-for="cat in categories"
+                    :key="cat"
+                    v-on:click="onChange(cat)"
+                    v-bind:class="{active : interests.indexOf(cat) !== -1}"
+                >{{ cat }}</li>
+            </ul>
         </div>
     </div>
 </template>
 
+<style lang="scss" scoped>
+    @import '~@/_variables.scss';
+    .interest-selector {
+        text-align: center;
+        p {
+            margin-top: 60px;
+        }
+        .categories {
+            list-style: none;
+            margin: 30px 0;
+            padding: 0;
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            li {
+                font-size: 12px;
+                margin: 0 5px 5px;
+                color: $primary;
+                font-weight: $w-bold;
+                padding: 6px 17px;
+                border-radius: 200px;
+                background: rgba($primary, 0.05);
+                text-transform: capitalize;
+                cursor: pointer;
+                user-select: none;
+                transition: all 300ms ease-in-out;
+                &.active {
+                    background: $primary;
+                    color: white;
+                }
+            }
+        }
+    }
+</style>
+
+
 <script>
+    import categories from '../../data/categories'
     export default {
-        name: 'RegisterEmail',
-        props: ['email'],
-        methods: {
-            onChange(e) {
-                this.$emit('onchange', e)
+        name: 'RegisterInterests',
+        props: ['interests'],
+        data() {
+            return {
+                categories: categories
             }
         },
-        computed: {
-            emailHasContent: function() {
-                return {
-                    'has-content': this.email
-                }
+        methods: {
+            onChange(cat) {
+                this.$emit('onpush', cat)
             }
         }
     }
