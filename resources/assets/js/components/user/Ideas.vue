4502f8642d260598a88dc59f582ddf0cc81c2762
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-02 23:06:30 +1100

    Uses store getter: getUserIdeas, to listen and render user ideas. Fixed profile swap with lingering data

--- resources/assets/js/components/user/Ideas.vue
+++ resources/assets/js/components/user/Ideas.vue
@@ -1,41 +1,31 @@
 <template>
   <section class="user-ideas">
     <div class="idea_wrapper">
-        <div v-if="user_ideas === ''"><loading/></div>
-        <div v-else v-for="(value, key) in this.user_ideas" :key="key">
-            <IdeaCard key={{key}} :props='value'/>
-        </div>
+
+        <template v-if="!userIdeas">
+          <h1>No Ideas</h1>
+        </template>
+
+        <template v-else>
+          <IdeaCard v-for="(value, key) in userIdeas" :key="key" :props='value'/>
+        </template>
+
     </div>
   </section>
 </template>
 
-<style lang="scss">
-@import '~@/_variables.scss';
-
-</style>
-
 <script>
-import IdeaCard from '../IdeaCard.vue'
-    export default {
-      name: 'Ideas',
-      components: {
-        IdeaCard
-      },
-      data() {
-        return {
-          user_ideas: '',
-        }
-      },
-      mounted() {
-        console.log("User Ideas");
-        this.getIdeaData();
-      },
-      methods: {
-        getIdeaData() {
-          this.user_ideas = this.$ud_store.state.current_user_ideas;
-          console.warn('this.$ud_store.state.current_user_view ', this.$ud_store.state.current_user_view.user_meta)
-          console.warn('this.$ud_store.state.current_user_ideas ', this.$ud_store.state.current_user_ideas)
-        }
+  import IdeaCard from '../IdeaCard.vue'
+  
+  export default {
+    name: 'Ideas',
+    components: {
+      IdeaCard
+    },
+    computed: {
+      userIdeas() {
+        return this.$ud_store.getters.getUserIdeas
       }
     }
+  }
 </script>
