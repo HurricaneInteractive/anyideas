e0c5c227a3608421c0e126928ea26d38a0815994
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-02 22:56:54 +1100

    Added RegisterInterest component to Register view / move styling to scss file / additional validation and sending registration request

--- resources/assets/js/views/Register.vue
+++ resources/assets/js/views/Register.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="register-view">
+    <div class="user-login-register-view">
         <div class="container">
             <header>
                 <router-link :to="{name: 'index'}" v-html="this.$ud_store.state.icons.logo_big"></router-link>
@@ -14,11 +14,14 @@
                 <template v-else-if="stepIndex === 3">
                     <RegisterPasswords v-on:onchange="updateParent" :password="password" :password_confirmation="password_confirmation" />
                 </template>
+                <template v-else-if="stepIndex === 4">
+                    <RegisterInterests v-on:onpush="pushInterest" :interests="interests" />
+                </template>
 
                 <p v-if="error !== null" class="error">{{ error.msg }}</p>
 
-                <a href="#" class="btn btn-register" v-on:click="stepChange">
-                    <template v-if="processing === false">
+                <a href="#" class="btn btn-register" v-bind:class="{ disabled: processing }" v-on:click="stepChange">
+                    <template v-if="processing === false && stepIndex !== 4">
                         Next <span class="arrow-right" v-html="this.$ud_store.state.icons.arrow_right" />
                     </template>
                     <template v-else-if="processing === false && stepIndex === 4">
@@ -40,183 +43,37 @@
     </div>
 </template>
 
-<style lang="scss" scoped>
-    @import '~@/_variables.scss';
-    .register-view {
-        background: $white;
-        min-height: 100vh;
-        padding: 30px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        .container {
-            max-width: 360px;
-            margin: 0 auto;
-            > header {
-                max-width: 170px;
-                margin: 0 auto 50px;
-                a, svg {
-                    display: block;
-                }
-            }
-            .steps-container {
-                padding: 50px 40px 85px;
-                background: white;
-                position: relative;
-                .btn {
-                    text-align: center;
-                    background: rgba($primary, 0.2);
-                    font-size: 1.2em;
-                    font-weight: $w-bold;
-                    color: $primary;
-                    display: block;
-                    max-width: 225px;
-                    width: 100%;
-                    margin: 0 auto;
-                    padding: 12px;
-                    text-decoration: none;
-                    border-radius: 5px;
-                    position: relative;
-                    .arrow-right {
-                        display: block;
-                        position: absolute;
-                        top: 50%;
-                        right: 15px;
-                        transform: translateY(-50%);
-                        line-height: 0;
-                        svg {
-                            display: block;
-                        }
-                    }
-                    .loading {
-                        width: 25px;
-                        height: 25px;
-                        > div {
-                            width: 25px !important;
-                            height: 25px !important;
-                            svg {
-                                fill: $primary !important;
-                            }
-                        }
-                    }
-                }
-                .dots {
-                    margin: 0;
-                    padding: 0;
-                    list-style: none;
-                    text-align: center;
-                    position: absolute;
-                    bottom: 17px;
-                    left: 50%;
-                    transform: translateX(-50%);
-                    li {
-                        display: inline-block;
-                        margin: 0 5px;
-                        width: 7px;
-                        height: 7px;
-                        border-radius: 50%;
-                        background: rgba($primary, 0.2);
-                        &.active {
-                            background: $primary;
-                        }
-                    }
-                }
-                .error {
-                    text-align: center;
-                    color: $error-color;
-                }
-            }
-        }
-    }
-</style>
-
 <style lang="scss">
-    @import '~@/_variables.scss';
-    .steps-container {
-        .step {
-            blockquote {
-                text-align: center;
-                font-size: 1.4em;
-                margin: 0;
-                > p {
-                    margin: 0;
-                }
-                cite {
-                    margin: 24px 0 0;
-                    font-style: normal;
-                    text-transform: uppercase;
-                    font-size: 0.6em;
-                    display: block;
-                }
-            }
-            .form-block {
-                margin: 50px 0 30px;
-                padding: 30px;
-                background: rgba($primary, 0.05);
-                .input-field {
-                    position: relative;
-                    &:not(:first-child) {
-                        margin-top: 40px;
-                    }
-                    input {
-                        padding: 0 10px 10px;
-                        background: transparent;
-                        border: none;
-                        border-bottom: 1px solid $primary;
-                        width: 100%;
-                        font-family: $font-family-sans-serif;
-                        font-size: 1em;
-                        &:focus {
-                            outline: none;
-                        }
-                        &:focus,
-                        &.has-content {
-                            & + label {
-                                top: calc(-100% + 10px);
-                                left: 0;
-                                font-size: 0.8em;
-                            }
-                        }
-                    }
-                    label {
-                        font-size: 1em;
-                        position: absolute;
-                        top: 0%;
-                        left: 10px;
-                        pointer-events: none;
-                        color: rgba($primary, 0.7);
-                        transition: all 200ms ease-in-out;
-                    }
-                }
-            }
-        }
-    }
+    @import '~@/_login-register.scss';
 </style>
 
 <script>
     import RegisterName from '../components/register/RegisterName'
     import RegisterEmail from '../components/register/RegisterEmail'
     import RegisterPasswords from '../components/register/RegisterPasswords'
+    import RegisterInterests from '../components/register/RegisterInterests'
 
     import {
         validName,
         validUsername,
         validEmail,
-        validPassword
+        validPassword,
+        validInterests
     } from '../components/register/helpers/validators'
 
     export default {
         components: {
             RegisterName,
             RegisterEmail,
-            RegisterPasswords
+            RegisterPasswords,
+            RegisterInterests
         },
         data(){
             return {
                 name : null,
                 username: null,
                 email: null,
-                interests: null,
+                interests: [],
                 password : null,
                 password_confirmation: null,
                 stepIndex: 1,
@@ -227,6 +84,18 @@
         methods : {
             updateParent(e) {
                 this[e.target.name] = e.target.value
+            },
+            pushInterest(cat) {
+                let index = this.interests.indexOf(cat)
+                if (index === -1) {
+                    if (this.interests.length === 3) {
+                        return false;
+                    }
+                    this.interests.push(cat)
+                }
+                else {
+                    this.interests.splice(index, 1);
+                }
             },
             returnValidateRegisterResponse(data, errorMsg) {
                 if (data.user !== null) {
@@ -300,12 +169,21 @@
                         break;
                     }
                     case 4: {
+                        let validatedInterests = validInterests(this.interests)
+                        if (validatedInterests.valid === false) {
+                            this.error = validatedInterests
+                            return false
+                        }
+
+                        // console.log('Submit that shizzle')
+                        this.processing = true
+                        this.handleSubmit();
                         break;
                     }
                 }
             },
-            handleSubmit(e) {
-                e.preventDefault()
+            handleSubmit() {
+                // e.preventDefault()
 
                 console.log(this.name)
                 console.log(this.username)
@@ -319,35 +197,28 @@
                 // data items need to be stringify'd before pushing to the DB
                 // return items need to be parsed back into arrays/objects
 
-                if (this.password === this.password_confirmation && this.password.length > 5) {
-                    axios({
-                        method: 'POST',
-                        data: {
-                            name: this.name,
-                            username: this.username,
-                            email: this.email,
-                            interests: this.interests,
-                            password: this.password,
-                            password_confirmation: this.password_confirmation
-                        },
-                        url: '/register'
-                    })
-                    .then(response => {
-                        console.log("register response")
-                        console.log(response)
-                        if (response.status === 200) {
-                            window.location = '/';
-                        }
-                    })
-                    .catch(error => {
-                        console.error(error);
-                    });
-                } else {
-                    this.password = ""
-                    this.password_confirmation = ""
-
-                    return alert('Password is not long enough')
-                }
+                axios({
+                    method: 'POST',
+                    data: {
+                        name: this.name,
+                        username: this.username,
+                        email: this.email,
+                        interests: JSON.stringify(this.interests),
+                        password: this.password,
+                        password_confirmation: this.password_confirmation
+                    },
+                    url: '/register'
+                })
+                .then(response => {
+                    console.log("register response")
+                    console.log(response)
+                    if (response.status === 200) {
+                        window.location = '/';
+                    }
+                })
+                .catch(error => {
+                    console.error(error);
+                });
             }
         }
     }
