115424eda01783c492ee063484e13e00fabcf506
Luke Secomb <luke.secomb@live.com>
2018-11-03 19:51:27 +1100

    dynamic funtions to initilise either editor or view on description page
    dynamic funtions to initilise either editor or view on description page

--- resources/assets/js/components/idea/Description.vue
+++ resources/assets/js/components/idea/Description.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="description fixed_width">
-    <div v-if="loaded" id="editSection" class="editSection"/>
+    <div id="editSection" class="editSection"/>
   </section>
 </template>
 
@@ -25,10 +25,51 @@
           loaded: false
         }
       },
+      computed: {
+        currentIdeaData() {
+          return this.$ud_store.getters.getCurrentIdea
+        },
+        getIdeaUserId() {
+          return this.$ud_store.getters.getCurrentIdeaUserId
+        },
+        currentUserData() {
+          return this.$ud_store.getters.getUserData
+        },
+        currentIdeaDescription() {
+          return this.$ud_store.getters.getCurrentIdeaDescription
+        }
+      },
+      watch: {
+        $route: function(to, from) {
+          if (to.params.id !== from.params.id) {
+              this.updateDescription();
+              // handle get more data here;
+          }
+        }
+      },
+      beforeMount() {
+        console.log("%c Description.vue beforeMount(start)", this.$ud_store.state.consoleLog.component)
+        console.log('this.$parent.isUsersIdea', this.$parent.isUsersIdea)
+        console.log('this.currentIdeaDescription', this.currentIdeaDescription)
+        if (this.$parent.isUsersIdea) {
+          // init editor function(
+          this.initialiseEditor()
+        } else {
+          this.initialiseViewer()
+          // init view  function()  
+        }
+        // this.initialiseDesc();
+        
+        console.log("%c Description.vue beforeMount(end)", this.$ud_store.state.consoleLog.component)
+      },
       mounted() {
-        console.log("%c Description.vue", this.$ud_store.state.consoleLog.component)
+        console.log("%c Description.vue mounted(start)", this.$ud_store.state.consoleLog.component)
+        console.log('this.$ud_store.getters.getUserData => ', )
         console.log("%c Description.vue (this.$parent.isUsersIdea)", this.$parent.isUsersIdea)
-        this.setDescription();
+        // console.log('editor => ', editor)
+        // this.updateDescription();
+        
+        console.log("%c Description.vue mounted(end)", this.$ud_store.state.consoleLog.component)
       },
       methods: {
         onBlur(val) {
@@ -37,32 +78,40 @@
           this.$parent.description_to_update = newContent;
           console.warn('this.$parent.description_to_update => ', this.$parent.description_to_update)
         },
-        setDescription() {
-          console.warn('run setDescription Func')
-          console.log(this.$parent.idea_data)
-          console.log("%c Description.vue (this.$parent.isUsersIdea)", this.$parent.isUsersIdea)
-          if (this.$parent.isUsersIdea) {
-            console.log('this.$parent.isUsersIdea TRUE')
-            var editor = new Editor({
-              el: document.querySelector('#editSection'),
-              initialEditType: 'wysiwyg',
-              previewStyle: 'vertical',
-              height: '740px',
-              initialValue: 'this.$parent.idea_data.description',
-              events: {
-                blur: () => this.onBlur(editor),
-              }
-            });
-          } else {
-            console.log('this.$parent.isUsersIdea FALSE')
-            var editor = Editor.factory({
-              el: document.querySelector('#editSection'),
-              viewer: true,
-              height: '740px',
-              initialValue: 'this.$parent.idea_data.description'
-            });
-          }
-          this.loaded = true;
+        initialiseViewer() {
+          console.log("%c initialiseViewer", this.$ud_store.state.consoleLog.component)
+          var editor = Editor.factory({
+            el: document.querySelector('#editSection'),
+            viewer: true,
+            initialValue: this.currentIdeaDescription,
+          });
+        },
+        initialiseEditor() {
+          console.log("%c initialiseEditor", this.$ud_store.state.consoleLog.component)
+          var editor = new Editor({
+            el: document.querySelector('#editSection'),
+            initialEditType: 'wysiwyg',
+            previewStyle: 'vertical',
+            height: '740px',
+            initialValue: this.currentIdeaDescription,
+            events: {
+              blur: () => this.onBlur(editor)
+            }
+          });
+        },
+        updateDescription() {
+          console.warn('run setDescription Func', this.$ud_store.getters.getCurrentIdea)
+          editor = new Editor({
+            el: document.querySelector('#editSection'),
+            initialEditType: 'wysiwyg',
+            previewStyle: 'vertical',
+            height: '740px',
+            initialValue: 'this.currentIdeaDescription',
+            // initialValue: this.$ud_store.getters..description,
+            events: {
+              blur: () => this.onBlur(editor)
+            }
+          });
         },
       }
     }
