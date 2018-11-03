cb4fdcf993254bee044b1a37819086332d398875
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-03 22:50:22 +1100

    Added user bio with toggle-able edit editor / bio empty state

--- resources/assets/js/components/user/About.vue
+++ resources/assets/js/components/user/About.vue
@@ -39,15 +39,32 @@
         </ul>
       </div>
 
-      <div v-if="userOwns" class="editSection" id="editSection"/>
-      <div v-else id="viewerSection"/>
+      <div class="bio-wrapper">
+        <h3>Bio
+          <template v-if="showEditingControls">
+            <template v-if="!editing_bio">
+              <a href="#edit-interests" @click="toggleBioEditing"><span v-html="this.$ud_store.state.icons.edit" /></a>
+            </template>
+            <template v-else>
+              <a href="#edit-interests" @click="toggleBioEditing"><span v-html="this.$ud_store.state.icons.x" /></a>
+              <a href="#save-interests" @click="saveUserBio"><span v-html="this.$ud_store.state.icons.check" /></a>
+            </template>
+          </template>
+        </h3>
+        <div class="editor" ref="userBio" />
+        <div v-if="!showEditingControls && !userHasBio" class="no-bio">
+          <span v-html="this.$ud_store.state.icons.no_bio" />
+          <h2>Definitely still writing.</h2>
+        </div>
+      </div>
+
     </div>
     
   </section>
 </template>
 
 <style lang="scss">
-@import '~@/App.scss';
+@import '~@/_variables.scss';
 
 .user_stats {
   width: 250px;
@@ -81,6 +98,15 @@
     }
   }
 }
+.bio-wrapper {
+  .no-bio {
+    span {
+      svg g * {
+        stroke: $grey-med !important;
+      }
+    }
+  }
+}
 /* .about {
   .about-wrapper {
     max-width: 760px;
@@ -97,8 +123,8 @@
       padding-top: 60px;
     }
   }
-  .interests-wrapper {
-    text-align: center;
+  .interests-wrapper,
+  .bio-wrapper {
     h3 {
       margin: 0 0 25px;
       display: inline-flex;
@@ -110,6 +136,9 @@
         color: $black;
       }
     }
+  }
+  .interests-wrapper {
+    text-align: center;
     .interests {
       display: flex;
       justify-content: center;
@@ -150,16 +179,41 @@
       }
     }
   }
+  .bio-wrapper {
+    margin-top: 60px;
+    h3 {
+      justify-content: center;
+      display: flex;
+    }
+    .no-bio {
+      text-align: center;
+      span {
+        max-width: 250px;
+        display: block;
+        margin: 0 auto;
+      }
+      h2 {
+        margin: 30px 0 10px;
+      }
+    }
+  }
 </style>
 
 <script>
   import categories from '../../data/categories'
-  // require('codemirror/lib/codemirror.css') // codemirror
-  // require('tui-editor/dist/tui-editor.css'); // editor ui
-  // require('tui-editor/dist/tui-editor-contents.css'); // editor content
-  // require('highlight.js/styles/github.css'); // code block highlight
+  
+  require('codemirror/lib/codemirror.css') // codemirror
+  require('tui-editor/dist/tui-editor.css'); // editor ui
+  require('tui-editor/dist/tui-editor-contents.css'); // editor content
+  require('highlight.js/styles/github.css'); // code block highlight
 
-  // var Editor = require('tui-editor');
+  // deps for viewer.
+  require('tui-editor/dist/tui-editor-contents.css'); // editor content
+  require('highlight.js/styles/github.css'); // code block highlight
+
+  var Viewer = require('tui-editor/dist/tui-editor-Viewer');
+  var Editor = require('tui-editor');
+
   export default {
     name: 'About',
     props: ['data'],
@@ -169,12 +223,22 @@
         loaded: false,
         editing_interests: false,
         interests: [],
-        categories: categories
+        categories: categories,
+        editor: null,
+        viewInitialsed: false,
+        editorInitialsed: false,
+        editing_bio: false,
+        bio: ''
       }
     },
     beforeMount() {
     },
     mounted() {
+      if (this.userMeta && this.userMeta.user_meta) {
+        if (this.userMeta.user_meta.bio) {
+          this.inititaliseViewer();
+        }
+      }
     },
     methods: {
       toggleEditing(e) {
@@ -188,6 +252,23 @@
         }
         else {
           this.interests = []
+        }
+      },
+      toggleBioEditing(e) {
+        e.preventDefault();
+        this.editing_bio = !this.editing_bio
+        this.viewInitialsed = false
+        this.editorInitialsed = false
+
+        if (this.editing_bio === true) {
+          this.bio = this.userMeta.user_meta ? this.userMeta.user_meta.bio : ''
+          this.inititaliseEditor();
+        }
+        else {
+          this.bio = ''
+          if (this.userMeta.user_meta) {
+            this.inititaliseViewer();
+          }
         }
       },
       onInterestClick(e, cat) {
@@ -228,6 +309,86 @@
           }
         })
         .catch(e => console.error(e))
+      },
+      getEditorElem() {
+        let el = this.$refs.userBio
+        el.innerHTML = ''
+        el.removeAttribute('style')
+
+        return el
+      },
+      inititaliseEditor() {
+        if (this.editorInitialsed) return false
+
+        let el = this.getEditorElem()
+        if (!el) return false
+
+        const $this = this;
+        
+        this.editor = new Editor({
+          el: el,
+          initialEditType: 'markdown',
+          previewStyle: 'vertical',
+          height: '500px',
+          usageStatistics: false,
+          initialValue: this.bio,
+          events: {
+            change() {
+              $this.editorOnChange()
+            }
+          }
+        })
+
+        this.editorInitialsed = false
+      },
+      inititaliseViewer() {
+        if (this.viewInitialsed) return false
+
+        let el = this.getEditorElem()
+
+        if (!el) return false
+
+        const $this = this;
+
+        this.editor = new Viewer({
+          el: el,
+          height: 'auto',
+          initialValue: $this.userMeta.user_meta.bio
+        })
+
+        this.viewInitialsed = true
+      },
+      editorOnChange() {
+        console.log(this.editor.getValue());
+        this.bio = this.editor.getValue()
+      },
+      saveUserBio(e) {
+        e.preventDefault();
+
+        if (!this.showEditingControls) return false;
+
+        axios({
+          method: 'POST',
+          url: `/ai/user/update.meta/${this.userMeta.user.id}`,
+          data: {
+            user_meta_update: {
+              bio: this.editor.getValue()
+            }
+          }
+        })
+        .then(({data}) => {
+          if (!data.success) {
+            alert(data.msg)
+            return false
+          }
+          else {
+            let bio = data.update.bio || ''
+            
+            this.$ud_store.commit('SET_CURRENT_USER_BIO', data.update.bio);
+            this.toggleBioEditing(e);
+          }
+        })
+        .catch(e => console.error(e))
       }
     },
     computed: {
@@ -240,8 +401,10 @@
       countFollowers() {
         if (this.userMeta && this.userMeta.user_meta) {
           if (this.userMeta.user_meta.hasOwnProperty('followers')) {
-            let follower = JSON.parse(this.userMeta.user_meta.followers)
-            return follower.length
+            if (this.userMeta.user_meta.followers !== null) {
+              let follower = JSON.parse(this.userMeta.user_meta.followers)
+              return follower.length
+            }
           }
         }
 
@@ -254,6 +417,27 @@
         }
         else {
           return false
+        }
+      },
+      userHasBio() {
+        if (this.userMeta && this.userMeta.user_meta) {
+          let meta = this.userMeta.user_meta
+          if (meta.hasOwnProperty('bio')) {
+            if (meta.bio !== null && typeof meta.bio !== 'undefined' && meta.bio.trim() !== '') {
+              return true
+            }
+          }
+        }
+
+        return false;
+      }
+    },
+    watch: {
+      userMeta: function(val) {
+        if (this.viewInitialsed) return false
+
+        if (val.user_meta && val.user_meta.bio) {
+          this.inititaliseViewer();
         }
       }
     }
