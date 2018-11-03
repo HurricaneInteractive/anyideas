9feaf70506dd9d630d0458fbd941892fe4ab4fae
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-03 16:03:05 +1100

    Show different no results graphics based on user profile ownership

--- resources/assets/js/components/user/Ideas.vue
+++ resources/assets/js/components/user/Ideas.vue
@@ -3,7 +3,16 @@
     <div class="idea_wrapper">
 
         <template v-if="!userIdeas">
-          <h1>No Ideas</h1>
+          <template v-if="isCurrentUsersProfile">
+            <div class="no-ideas">
+              <span v-html="this.$ud_store.state.icons.profile_no_ideas" />
+              <h2>Share your ideas now</h2>
+              <router-link :to="{name: 'add-new-idea'}" class="btn btn-new">Post Idea</router-link>
+            </div>
+          </template>
+          <template v-else>
+            <noideas />
+          </template>
         </template>
 
         <template v-else>
@@ -13,6 +22,52 @@
     </div>
   </section>
 </template>
+
+<style lang="scss" scoped>
+  @import '~@/_variables.scss';
+  .no-ideas-found {
+    margin-top: 60px;
+  }
+  .no-ideas {
+    margin-top: 60px;
+    text-align: center;
+    span {
+      max-width: 400px;
+      display: block;
+      margin: 0 auto;
+    }
+    h2 {
+      margin: 30px 0 10px;
+    }
+    .btn {
+      color: white;
+      font-weight: $w-bold;
+      background: $primary;
+      padding: 10px 30px;
+      border-radius: 200px;
+      text-transform: capitalize;
+      text-decoration: none;
+      display: inline-block;
+      transition: background 250ms ease-in-out;
+      &:hover {
+        background: lighten($primary, 10%);
+      }
+    }
+  }
+</style>
+
+<style lang="scss">
+  @import '~@/_variables.scss';
+  .no-ideas {
+    span {
+      svg g * {
+        stroke: $grey-med !important;
+      }
+    }
+  }
+</style>
+
+
 
 <script>
   import IdeaCard from '../IdeaCard.vue'
@@ -25,6 +80,17 @@
     computed: {
       userIdeas() {
         return this.$ud_store.getters.getUserIdeas
+      },
+      isCurrentUsersProfile() {
+        let user = this.$ud_store.getters.getUserData
+        
+        if (user !== null) {
+          if (parseInt(user.id) === parseInt(this.$route.params.id)) {
+            return true;
+          }
+        }
+
+        return false
       }
     }
   }
