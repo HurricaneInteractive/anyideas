d0779e06d51971f9478cf8299efcb9ad3a5d4f62
Luke Secomb <luke.secomb@live.com>
2018-11-04 22:29:57 +1100

    dynamic render by user_id / styling

--- resources/assets/js/components/idea/Updates.vue
+++ resources/assets/js/components/idea/Updates.vue
@@ -1,40 +1,27 @@
 <template>
   <section class="updates fixed_width">
-    <h2>Updates Goes Below</h2>
-    <h1>Idea ID = {{this.$route.params.id}}</h1>
-
-    <section>
-      <div>
-        <h3>Updates data</h3>
+      <div class="timeline_item_container sub_width">
         <!-- <button @click="hanldeGetUpdateData">get update_posts data from (pre filled) idea_id</button><br/><br/>    -->
     
-        <form method="POST">
-            <div class="form-group row">
-                <label for="update_post.title" class="col-md-4 col-form-label text-md-right">Title</label>
-
-                <div class="col-md-6">
-                <input id="update_post.title" type="text" class="form-control" v-model="update_post.title" required autofocus>
+        <div v-if="$parent.isUsersIdea" class="timeline_item timeline_add_new">
+            <div class="timeline_box">
+                <h5 class="timeline_date">Day / Month / Year</h5>
+                <label for="update_post.title">Title</label>
+                <input class="timeline_title" placeholder="Milestone Title" v-model="update_post.title" type="text" required/>
+                <label for="update_post.message">Description</label>
+                <textarea placeholder="Description (240 characters)" class="timeline_message" v-model="update_post.message" type="text" required/>
+                <div class="timeline_item_footer">
+                    <span></span>
+                    <div class="total-likes" @click="handleUpdatePostSubmit">
+                        <p>Post</p>
+                    </div>
                 </div>
             </div>
-            <div class="form-group row">
-                <label for="update_post.message" class="col-md-4 col-form-label text-md-right">Message</label>
+        </div>
 
-                <div class="col-md-6">
-                <input id="update_post.message" type="text" class="form-control" v-model="update_post.message" required>
-                </div>
-            </div>
-            <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary" @click="handleUpdatePostSubmit">
-                    Post update_post update
-                </button>
-                </div>
-            </div>
-        </form>
-
-        <div id="updates_post_data">
-            <!-- @{{ updates_post_data }} -->
-            <ul v-for="(value, key) in this.updates_post_data" :key="key">
+        <!-- <div v-if="getUpdatesData" id="updates_post_data">
+            <p>{{getUpdatesData}}</p>
+            <ul v-for="(value, key) in this.getUpdatesData" :key="key">
                 <li key={{key}}>
                     <h4>{{value.title}}</h4>
                     <p>{{value.message}}</p>
@@ -42,48 +29,191 @@
                     <button @click="handleUpdatePostDarts(value.id)">{{value.darts}}</button>
                 </li>
             </ul>
+        </div> -->
+
+        <div class="timeline_item" v-for="(value, key) in this.getUpdatesData" :key="key">
+            <div class="timeline_box">
+                <h5 class="timeline_date">{{(new Date(value.created_at).getDate()) + '/' + (new Date(value.created_at).getMonth() + 1) + '/' + (new Date(value.created_at).getFullYear())}}</h5>
+                
+                <h2 class="timeline_title">
+                    <a v-if="value.link !== null" class="timeline_link" :href="value.link" target="_blank">{{value.title}}</a>
+                    <span v-else >{{value.title}}</span>
+                </h2>
+                <p class="timeline_message">{{value.message}}</p>
+                <div class="timeline_item_footer">
+                    <span></span>
+                    <!-- <span class="timeline_button" @click="handleTimelineSubmit" v-html="icons.dart"></span> -->
+                    <div v-if="$parent.isUsersIdea" class="delete total-likes"  @click="handleTimelineDelete(value.id)">
+                        <p>Delete</p><span v-html="icons.trash" />
+                    </div>
+                    <div v-else class="total-likes" @click="handleUpdatePostDarts(value.id)">
+                        <p>{{ value.darts }}</p><span v-html="icons.dart" />
+                    </div>
+                    
+                </div>
+            </div>
         </div>
-
-        <h4>update_post item</h4>
-        <form method="POST">
-            <div class="form-group row">
-                <label for="update_post_update.id" class="col-md-4 col-form-label text-md-right">update_post id to update</label>
-
-                <div class="col-md-6">
-                <input id="update_post_update.id" type="text" class="form-control" v-model="update_post_update.id" required autofocus>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="update_post_update.title" class="col-md-4 col-form-label text-md-right">Title</label>
-
-                <div class="col-md-6">
-                <input id="update_post_update.title" type="text" class="form-control" v-model="update_post_update.title" required autofocus>
-                </div>
-            </div>
-            <div class="form-group row">
-                <label for="update_post_update.message" class="col-md-4 col-form-label text-md-right">Message</label>
-
-                <div class="col-md-6">
-                <input id="update_post_update.message" type="text" class="form-control" v-model="update_post_update.message" required>
-                </div>
-            </div>
-            <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-4">
-                <button type="submit" class="btn btn-primary" @click="handleUpdatePostUpdate">
-                    Update updates_item
-                </button>
-                </div>
-            </div>
-        </form>
     </div>
-    </section>
   </section>
 </template>
 
 <style lang="scss">
 @import '~@/_variables.scss';
 
-
+.timeline_item_container {
+    .timeline_add_new {
+        .timeline_box {
+            display: grid;
+            font-family: $font-family-sans-serif;
+            .timeline_title {
+                margin: 0 0 16px !important;
+            }
+            .timeline_message {
+                min-height: 96px;
+                max-width: 100%;
+                margin-bottom: 24px;
+            }
+            label {
+                display: none;
+            }
+            input, textarea {
+                border: none;
+                background-color: transparent;
+                padding-bottom: 2px;
+                border-bottom: 1px solid transparent;
+                transition: .25s;
+                &:focus {
+                    border: none;
+                    outline: none;
+                    border-bottom: 1px solid $black;
+                    transition: .5s;
+                }
+            }
+        }
+    }
+    .timeline_item {
+        display: grid;
+        grid-template-columns: 1fr;
+        .timeline_box {
+            padding: 16px 0;
+            width: 100%;
+            .timeline_date {
+                color: $grey-med;
+                margin: 0;
+                font-size: .83rem;
+            }
+            .timeline_title {
+                position: relative;
+                color: $black;
+                margin: 0 0 36px;
+                font-size: 1.5rem;
+                font-weight: $w-bold;
+                a, span {
+                    color: $black;
+                    font-size: 1.5rem;
+                    font-weight: $w-bold;
+                }
+            }
+            .timeline_message {
+                color: $grey-dark;
+                font-size: 1rem;
+            }
+            .timeline_item_footer {
+                display: grid;
+                grid-template-columns: 1fr 1fr;
+                grid-template-rows: 1fr;
+                .delete {
+                    background-color: $black !important;
+                    p {
+                        color: $white;
+                    }
+                    span {
+                        margin-left: 4px;
+                        padding: 4px 0 4px 4px;
+                        width: 10px;
+                        fill: $white;
+                        span svg {
+                            display: block;
+                        }
+                    }
+                }
+                .total-likes {
+                    justify-self: flex-end;
+                    padding: 5px 15px;
+                    background: white;
+                    border-radius: 200px;
+                    line-height: 1;
+                    display: inline-flex;
+                    justify-content: center;
+                    align-items: center;
+                    box-shadow: 0 3px 6px 0 rgba($black, 0.16);
+                    p {
+                        margin: 0 auto;
+                        padding: 4px 0;
+                        flex-grow: 2;
+                        font-size: .9rem;
+                        font-weight: $w-bold;
+                    }
+                    span {
+                        width: 22px;
+                        span svg {
+                            display: block;
+                            fill: $white;
+                        }
+                    }
+                }
+                .timeline_link {
+                    display: flex;
+                    align-self: center;
+                    justify-self: flex-start;
+                    margin: 0;
+                    padding: 0;
+                    color: $black;
+                    padding: 8px 16px;
+                    margin: 0 4px;
+                    border-radius: 25px;
+                    background-color: $white;
+                    border: none;
+                    box-shadow: 0 3px 6px 0 rgba($black, 0.16);
+                    transition: .5s;
+                    &:hover {
+                        transition: .25s;
+                        box-shadow: 0 6px 9px 0 rgba($black, 0.16);
+                    }
+                    span {
+                        width: 12px;
+                        display: flex;
+                        color: $grey-med;
+                        &:nth-of-type(2) {
+                            cursor: pointer;
+                        }
+                    }
+                    input {
+                        margin: 0 8px;
+                    }
+                }
+                .timeline_button {
+                    align-self: center;
+                    justify-self: flex-end;
+                    /* border: 1px solid $black; */
+                    color: $black;
+                    padding: 8px 16px;
+                    margin: 0 4px;
+                    border-radius: 25px;
+                    background-color: $white;
+                    border: none;
+                    box-shadow: 0 3px 6px 0 rgba($black, 0.16);
+                    cursor: pointer;
+                    transition: .5s;
+                    &:hover {
+                        transition: .25s;
+                        box-shadow: 0 6px 9px 0 rgba($black, 0.16);
+                    }
+                }
+            }
+        }
+    }
+}
 </style>
 
 <script>
@@ -91,7 +221,7 @@
       name: 'Updates',
       data() {
           return {
-            updates_post_data: '',
+            updates_post_data: this.getUpdatesData,
             update_post: {
               title: '',
               message: ''
@@ -106,13 +236,45 @@
               title: '',
               message: ''
             },
+            icons: {
+                dart: this.$ud_store.state.icons.dart,
+                trash: this.$ud_store.state.icons.trash
+            }
           }
       },
-      mounted() {
+    computed: {
+        getUpdatesData() {
+            return this.$ud_store.getters.getCurrentIdeaUpdates;
+        }
+    },
+    watch: {
+        getUpdatesData: function(val) {
+            if (this.viewInitialsed) return false
+
+            // if (val.user_meta && val.user_meta.bio) {
+            //     this.inititaliseViewer();
+            // }
+            console.log('updates_post_data => ', this.updates_post_data)
+            console.log('parent.isUsersIdea => ', this.$parent.isUsersIdea)
+        }
+    },
+    mounted() {
         console.log("%c Updates.vue", this.$ud_store.state.consoleLog.component)
-      },
+        this.hanldeGetUpdateData();
+        console.log('this.$parent => ', this.$parent)
+    },
       methods: {
         // updates post functions
+        hanldeGetUpdateData() {
+            axios({
+                method: 'POST',
+                url: '/ai/idea/update_post/get/all/' + this.$route.params.id
+            }).then( (res) => {
+                console.log('TCL: hanldeGetUpdateData -> res.data', res.data);
+                this.updates_post_data = res.data;
+                this.$ud_store.commit('SET_IDEA_UPDATES', res.data)
+            }); 
+        },
         handleUpdatePostDarts(value) {
             axios({
                 method: 'POST',
@@ -120,16 +282,10 @@
                 headers: {
                     'X-CSRF-TOKEN': document.querySelector("meta[name='csrf-token']").getAttribute('content')
                 }
+            }).then(res => {
+                console.log('res => ', res.data);
+                this.hanldeGetUpdateData()
             });
-        },
-        hanldeGetUpdateData(e) {
-            e.preventDefault();
-            axios({
-                method: 'POST',
-                url: '/ai/idea/update_post/get/all/' + this.$route.params.id
-            }).then( (response) => {
-                this.updates_post_data = response.data;
-            }); 
         },
         handleUpdatePostSubmit(e) {
             e.preventDefault();
@@ -140,16 +296,17 @@
                     title: this.update_post.title,
                     message: this.update_post.message
                 }
-            }).then( (response) => {
-                this.update_post_create = response.data;
+            }).then( (res) => {
+                this.update_post_create = res.data;
+                this.hanldeGetUpdateData()
             });
         },
         handleUpdatePostDelete(value) {
             axios({
                 method: 'POST',
                 url: '/ai/idea/update_post/delete/' + value,
-            }).then( (response) => {
-                this.update_post_create = response.data;
+            }).then( (res) => {
+                this.update_post_create = res.data;
             });
         },
         handleUpdatePostUpdate(e){
@@ -162,8 +319,8 @@
                     title: this.update_post_update.title,
                     message: this.update_post_update.message,
                 },
-            }).then( (response) => {
-                if (response.data === "") {
+            }).then( (res) => {
+                if (res.data === "") {
                     alert('error creating timeline entry');
                 }
             });
