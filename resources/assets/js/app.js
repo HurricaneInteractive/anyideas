12a6424efc08122dbee332c85368781288141b14
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-06 13:12:36 +1100

    File clean up

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,13 +1,10 @@
 window.axios = require('axios')
-// window.axios.defaults.baseURL = 'https://anyideas.herokuapp.com'
 
 import Vue from 'vue';
 import Vuex from 'vuex';
 import router from './routes.js';
-// import VueCookie from 'vue-cookie';
 import ud_store from './store';
 import VueClipboard from 'vue-clipboard2'
-// import './components/global/_globals'
 import App from './App.vue';
 import vSelect from 'vue-select'
 import InputTag from 'vue-input-tag'
@@ -18,7 +15,6 @@ import NoIdeasComp from './plugins/noideas.js'
 
 import VueAnime from './plugins/vue-anime';
 import capitalise from './plugins/capitalise';
-require('typeface-pt-sans')
 
 Vue.use( Vuex )
 // Vue.use( VueCookie )
@@ -39,53 +35,6 @@ Vue.component('v-select', vSelect)
 // ud_store
 // anime
 // capitalise
-
-/**
- * @deprecated 03/11/18
-router.beforeEach((to, from, next) => {
-    // if (to.name !== 'ideas' || to.name !== 'about') {
-    //     window.scrollTo(0, 0);
-    // }
-
-    console.log('to.path => ', to.name)
-
-    console.log('window.checkAuth => ', window.checkAuth);
-    console.log(' ud_store.state.data.user_data => ', ud_store.state.data.user_data);
-    // console.log(' ud_store.state.data.user_data => ', ud_store.state.data.user_data.id);
-
-    // check if user has logged out 
-    // if (window.checkAuth === undefined) {
-    //     console.log('undefined auth')
-    //     ud_store.commit('SET_USER_DATA', 'guest');
-    //     ud_store.commit('SET_USER_LOGGED_IN', false);
-    // } else {
-    //     console.log('defined auth')
-    //     ud_store.commit('SET_USER_DATA', window.checkAuth);
-    //     ud_store.commit('SET_USER_LOGGED_IN', true);
-    // }
-
-    // push category parmas to store for use on category page
-    if (to.name === 'category') {
-        ud_store.commit('SET_CATEGORY_ID', to.params.id );
-    }
-
-    if (to.fullPath !== "/login/guest") {
-        // if path is NOT /login/guest
-        if (typeof ud_store.state.data.user_data !== undefined) {
-            console.log('user_data !== undefined => ', ud_store.state)
-        }
-        if (ud_store.state.data.loggedIn === false) {
-            console.log('loggedIn === false => ', ud_store.state)
-        }
-        
-        next();
-    } else {
-        // alert('welcome to the login page');
-        next();
-    }
-
-});
- */
 
 router.beforeEach((to, from, next) => {
     let user = ud_store.state.data.user_data,
