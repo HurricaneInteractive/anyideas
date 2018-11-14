d3094736e316d204796c98cc1000bd04be46a7e5
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-14 17:23:48 +1100

    Separate our footer section into global component / clean up App.vue

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,146 +1,27 @@
 <template>
     <fragment>
-        <GlobalNavbar />
-        <CategoriesSlider v-if="this.$route.name === 'index' || this.$route.name === 'category'"/>
+        <GlobalNavbar v-if="!isLoginOrRegister" />
         <main>
             <router-view>
                 <!-- Child components run here -->
             </router-view>
         </main>
-        <footer class="footer-container" v-if="!isLoginOrRegister">
-            <div class="fixed_width">
-                <div class="footer footer-left">
-                    <ul>
-                        <li><router-link :to="{ name: 'index' }">News Feed</router-link></li>
-                        <li><router-link :to="{ name: 'index' }">Discover</router-link></li>
-                        <li><router-link :to="{ name: 'index' }">Success Stories</router-link></li>
-                        <li><router-link v-if="this.$ud_store.state.data.loggedIn !== false" :to="`/user/${$ud_store.state.data.user_data.id}`">Account</router-link></li>
-                        <li><router-link v-if="this.$ud_store.state.data.loggedIn !== false" :to="`/user/${$ud_store.state.data.user_data.id}`">Settings</router-link></li>
-                    </ul>
-                    <ul>
-                        <li><router-link :to="{ name: 'login' }">About</router-link></li>
-                        <li><router-link :to="{ name: 'login' }">Legalities</router-link></li>
-                    </ul>
-                    
-                    <ul>
-                        <li><a href="https://sunset-studios.netlify.com" target="_blank" rel="noopener noreferrer">Built by <span>Sunset Studios</span></a></li>
-                    </ul>
-                </div>
-                <div class="footer footer-right">
-                    <ul v-if="user_data === null">
-                        <li><router-link :to="{ name: 'login' }">Log In</router-link></li>
-                        <li><router-link :to="{ name: 'register' }">Sign Up</router-link></li>
-                    </ul>
-                    <ul class="social-media">
-                        <li><a href="https://facebook.com" target="_blank" rel="noopener noreferrer" v-html="this.$ud_store.state.icons.social.facebook" /></li>
-                        <li><a href="https://instagram.com" target="_blank" rel="noopener noreferrer" v-html="this.$ud_store.state.icons.social.instagram" /></li>
-                        <li><a href="https://youtube.com" target="_blank" rel="noopener noreferrer" v-html="this.$ud_store.state.icons.social.youtube" /></li>
-                    </ul>
-                </div>
-                <div class="footer-logo">
-                    <router-link :to="{name: 'index'}" v-html="this.$ud_store.state.icons.logo_big"></router-link>
-                </div>
-            </div>
-        </footer>
+        <GlobalFooter v-if="!isLoginOrRegister" />
     </fragment>
 </template>
-
-<!-- App.vue is imported into app.blade.php -->
 
 <style lang="scss">
     // Application Globals
     @import '~@/App.scss';
 </style>
 
-
-<style lang="scss" scoped>
-    @import '~@/_variables.scss';
-    .footer-container {
-        margin: 0 auto;
-        padding: 0 32px;
-        background-color: $white;
-        > div {
-            margin: 0 auto;
-            padding: 68px 32px 0;
-            display: grid;
-            grid-template-columns: 1fr 1fr;
-            .footer {
-                ul {
-                    list-style-type: none;
-                    padding: 0;
-                    margin: 0 0 32px 0;
-                    li {
-                        margin: 4px 0;
-                        a {
-                            color: $p-color;
-                            font-size: $size-h6;
-                            text-decoration: none;
-                        }
-                    }
-                }
-            }
-            .footer-right {
-                ul {
-                    li {
-                        text-align: right;
-                        a {
-                            text-align: right;
-                        }
-                    }
-                }
-                .social-media {
-                    a, svg {
-                        display: block;
-                        width: 21px;
-                        height: 21px;
-                        margin-left: auto;
-                    }
-                }
-            }
-            .footer-logo {
-                grid-area: 2 / 1 / 2 / 3;
-                margin: 24px auto 84px auto;
-                a {
-                    display: block;
-                    width: 128px;
-                    margin: 0 auto;
-                    svg {
-                        width: 100%;
-                    }
-                }
-            }
-        }
-    }
-</style>
-
 <script>
-import CategoriesSlider from './components/CategoriesSlider'
     export default {
-        components: {
-            CategoriesSlider
-        },
-        data(){
-            return {
-                openUserState: false,
-                openSearchState: false,
-                user_id : null,
-                name : null,
-                search_text: '',
-                search_results: {}
-            }
-        },
-        beforeMount: function() {
-        },
-        mounted: function(){
-            if (this.user_data === null) {
-                this.user_id = null;
-            } else {
-                this.user_id = this.user_data.id;
-                this.name = this.user_data.name;
-            }
-        },
+        data(){ return{} },
+        beforeMount() {},
+        mounted() {},
         methods: {
-            handleSignOut(e) {
+/*            handleSignOut(e) {
                 e.preventDefault();
                 this.menuState(false);
                 axios({
@@ -252,21 +133,9 @@ import CategoriesSlider from './components/CategoriesSlider'
                     console.error(error);
                 });
             },
+*/
         },
         computed: {
-            isUserProfilePage() {
-                return this.$route.matched.some(route => {
-                    return route.name === 'user' || (typeof route.parent !== 'undefined' && route.parent.name === 'user')
-                })
-            },
-            isIdeaPage() {
-                return this.$route.matched.some(route => {
-                    return route.name === 'add-new-idea' || route.name === 'idea'
-                })
-            },
-            user_data() {
-                return this.$ud_store.getters.getUserData
-            },
             isLoginOrRegister() {
                 return this.$route.matched.some(route => {
                     return route.name === 'login' || route.name === 'register'
