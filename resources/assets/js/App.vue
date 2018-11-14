22837b78789f3081692218b6546b3ffa44a0f9a5
HuricaneInteractive <hurricane.interactive@gmail.com>
2018-11-14 16:55:49 +1100

    Remove navbar

--- resources/assets/js/App.vue
+++ resources/assets/js/App.vue
@@ -1,57 +1,6 @@
 <template>
-    <div>
-        <nav class="navbar" v-bind:class="{profile: isUserProfilePage, addidea: isIdeaPage}" v-if="!isLoginOrRegister">
-            <div class="container fixed_width">
-                <ul class="navbar-wrapper navbar-left">
-                    <li><router-link :to="{name: 'index'}" v-html="this.$ud_store.state.icons.logo_small" class="logo-icon"/>
-                    </li>
-                </ul>
-                <!-- Right Side Of Navbar -->
-                <ul class="navbar-wrapper navbar-right">
-                    <li class="search">
-                        <li class="search_text_container">
-                            <div :class="'search_expand ' + this.openSearchState">
-                                <span @click="handleSearch" v-html="this.$ud_store.state.icons.search" class="navbar-icon"/>
-                                <input @keyup.enter="handleSearch" placeholder="search" value="search" id="search_text" type="text" class="form-control" v-model="search_text" />
-                            </div>
-                        </li>
-                        <template v-if="this.openSearchState === false">
-                            <li class="navbar-icon" v-on:click="openSearch()" v-html="this.$ud_store.state.icons.search">
-                            </li>
-                        </template>
-                        <template v-else>
-                            <li class="navbar-icon" v-on:click="openSearch()" v-html="this.$ud_store.state.icons.marks_the_spot">
-                            </li>
-                        </template>
-                    </li>
-                    <li>
-                        <span :class="'navbar-icon ' + this.$ud_store.state.data.loggedIn" v-on:click="openUser()" v-html="this.$ud_store.state.icons.user">
-                        </span>
-                        <div v-if="$ud_store.state.data.loggedIn === false" :class="'user ' + this.openUserState">
-                            <div class="user_items" v-on:click="openUser()">
-                                <router-link v-on:click="menuState(false)" :to="{name: 'login' }">Log In</router-link>
-                                <router-link v-on:click="menuState(false)" :to="{name: 'register' }">Sign Up</router-link>
-                            </div>
-                        </div>
-                        <div v-else :class="'user ' + this.openUserState">
-                            <div class="user_items" v-on:click="openUser()">
-                                <router-link :to="`/user/${$ud_store.state.data.user_data.id}`">View Account</router-link>
-                                <a href="#logout" @click="handleSignOut">Sign Out</a>
-                            </div>
-                        </div>
-                    </li>
-
-                    <li>
-                        <template v-if="this.$ud_store.state.data.loggedIn === false">
-                            <BaseButton :to="{name: 'register'}" raised text="Sign Up" />
-                        </template>
-                        <template v-else>
-                            <BaseButton :to="{name: 'add-new-idea'}" text="Idea" raised />
-                        </template>
-                    </li>
-                </ul>
-            </div>
-        </nav>
+    <fragment>
+        <GlobalNavbar />
         <CategoriesSlider v-if="this.$route.name === 'index' || this.$route.name === 'category'"/>
         <main>
             <router-view>
@@ -93,237 +42,19 @@
                 </div>
             </div>
         </footer>
-    </div>
+    </fragment>
 </template>
 
 <!-- App.vue is imported into app.blade.php -->
 
 <style lang="scss">
+    // Application Globals
     @import '~@/App.scss';
+</style>
 
-    // App.vue specific
-    .navbar-wrapper {
-        > li {
-            .user_icon {
-                cursor: pointer;
-            }
-            svg {
-                width: 25px;
-            }
-        }
-    }
 
-    .navbar {
-        &.profile,
-        &.addidea {
-            background: $white;
-        }
-        .navbar-icon {
-            fill: none;
-            color: $black;
-            stroke: $black;
-            cursor: pointer;
-        }
-        .container {
-            display: flex;
-            padding: 35px 0;
-            min-height: 64px;
-            .navbar-wrapper {
-                list-style-type: none;
-                padding: 0;
-                margin: 0;
-                align-content: center; 
-                li {
-                    margin: 0 16px;
-                }
-            }
-            .navbar-left {
-                width: 25%;
-                display: inline-flex;
-                justify-content: flex-start;
-                > li {
-                    display: inline-flex;
-                    a {
-                        display: grid;
-                        align-content: center;
-                    }
-                    div {
-                        padding-left: 16px;
-                    }
-                }
-            }
-            .navbar-left.true {
-                border-bottom: 2px solid black;
-            }
-            .navbar-right {
-                width: 75%;
-                display: inline-flex;
-                justify-content: flex-end;
-                .search_text_container {
-                    opacity: 0;
-                    display: inline-flex;
-                    align-content: center;
-                    justify-content: center;
-                    border-bottom: 2px black solid;
-                    margin: 0;
-                    padding: 0 8px;
-                    .search_expand {
-                        width: 80%;
-                        white-space: nowrap;
-                        display: flex;
-                        align-content: center;
-                        align-items: center;
-                        justify-content: center;
-                        input {
-                            margin: 0 24px;
-                            padding: 0;
-                            height: 100%;
-                            width: 100%;
-                            border: none;
-                        }
-                    }
-                }
-                .search {
-                    position: relative;
-                    .search_icon {
-                        display: grid;
-                        justify-content: center;
-                        align-content: center;
-                        svg {
-                            display: grid;
-                            justify-content: center;
-                            align-content: center;
-                        }
-                    }
-                }
-                > li {
-                    display: grid;
-                    justify-content: center;
-                    align-content: center;
-                    position: relative;
-                    z-index: 10;
-                    a {
-                        align-self: center;
-                        justify-self: center;
-                    }
-                    .post-idea {
-                        background-color: $primary;
-                        color: $pure;
-                        border: none;
-                        p {
-                            margin: 8px;
-                            color: $pure;
-                        }
-                    }
-                    .add-idea {
-                        // border: 1px solid $black;
-                        box-shadow: 0 3px 6px 0 rgba($black, 0.16);
-                        background: $pure;
-                    }
-                    .add-idea, .post-idea {
-                        display: inline-flex;
-                        align-content: center;
-                        justify-content: center;
-                        border-radius: 25px;
-                        padding: 2px 16px;
-                        text-decoration: none;
-                        > span {
-                            display: grid;
-                            align-content: center;
-                            justify-content: center;
-                            svg {
-                                width: 18px;
-                                height: 18px;
-                            }
-                        }
-                        p {
-                            margin: 10px 8px;
-                        }
-                    }
-                    .user_icon {
-                        display: grid;
-                        align-content: center;
-                        justify-content: flex-start;
-                        fill: none;
-                    }
-                    .user_icon.false {
-                        opacity: .5;
-                    }
-                    .user.false {
-                        display: none;
-                    }
-                    .user {
-                        position: absolute;
-                        // top: 15px;
-                        top: 100%;
-                        // width: 128px;
-                        // height: 128px;
-                        z-index: -100;
-                        left: calc(50% - 100px);
-                        opacity: 0;
-                        // transform: translateX(-50%);
-                        .arrow {
-                            position: relative;
-                            bottom: -12%;
-                            height: 30px;
-                            z-index: 100;
-                            display: flex;
-                            z-index: -100;
-                            fill: $white;
-                            stroke: $white;
-                            svg {
-                                width: 100%;
-                                z-index: -100;
-                                > * {
-                                    height: 15px;
-                                    width: 15px;
-                                    fill: $white;
-                                    stroke: $white;
-                                    border: 1px solid black;
-                                    border-color: transparent transparent $white $white;
-                                    z-index: -100;
-                                }
-                            }
-                        }
-                        a {
-                            margin: 14px auto;
-                            color: $p-color;
-                        }
-                        .user_items {
-                            z-index: 100;
-                            // height: 100%;
-                            min-width: 200px;
-                            width: 100%;
-                            position: relative;
-                            background-color: $white;
-                            opacity: 1;
-                            // display: grid;
-                            // align-content: center;
-                            // justify-content: center;
-                            border-radius: 10px;
-                            box-shadow: 0px 0px 15px 0px rgba(137, 137, 137, 0.25);
-                            padding: 30px;
-                            p {
-                                cursor: pointer;
-                                text-decoration: underline;
-                            }
-                            a, p {
-                                display: block;
-                                text-align: center;
-                                margin: 0;
-                                &:not(:first-child) {
-                                    padding-top: 15px;
-                                    margin-top: 15px;
-                                    border-top: 1px solid rgba($black, 0.05);
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    }
-
+<style lang="scss" scoped>
+    @import '~@/_variables.scss';
     .footer-container {
         margin: 0 auto;
         padding: 0 32px;
@@ -379,7 +110,7 @@
                 }
             }
         }
-    }    
+    }
 </style>
 
 <script>
